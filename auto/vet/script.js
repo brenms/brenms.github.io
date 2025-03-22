d6e885e3e6f4a350d037d234930c3c88023f324b
brenms <bmskeen@gmail.com>
2025-03-22 02:29:45 -0400

    Update script.js

--- auto/vet/script.js
+++ auto/vet/script.js
@@ -2,5 +2,5 @@ function selectAge(ageRange) {
     
     
     // Redirect to Google.com
-    window.location.href = 'https://www.gbencvm9trk.com/B4H89W/GTSC3/?sub2={clickid}&sub4={sub11}&sub3={campaignname}&sub5={ref_id}';
+    window.location.href = 'https://www.gbencvm9trk.com/B4H89W/GTSC3/?source_id={source_id}&sub1={clickid}';
 }
